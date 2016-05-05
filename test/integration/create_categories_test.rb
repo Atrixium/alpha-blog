d10f6e559e4879866a2fe1f9bf7d73b737f76691
Rob <rob.ss.bass@gmail.com>
2016-05-05 04:25:11 +0000

    Add article and cetgory association

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,17 +1,18 @@
 require 'test_helper'
 
-def setup
-  @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
-end
-
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+  @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+
   test "get new category form and create category" do
     sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
       post_via_redirect categories_path, category: {name:"Sports"}
-    end
+      end
     assert_template 'categories/index'
     assert_match "sports", response.body
   end
